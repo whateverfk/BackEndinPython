2516245a6a7eb641f7aea2843227c56c6a578b29
NAD <dinhcuadinhmain@gmail.com>
2026-01-22 15:26:00 +0700

    Font

---
+++ bePy/dist/config.ts
@@ -0,0 +1,11 @@
+/**
+ * Cấu hình hệ thống
+ * Bạn có thể thay đổi Domain API tại đây để áp dụng cho toàn bộ dự án.
+ */
+export const API_CONFIG = {
+  // Thay đổi URL này khi deploy hoặc kết nối với backend
+
+  //BASE_URL: 'http://localhost:3000',
+  BASE_URL: '',
+  TIMEOUT: 5000,
+}
